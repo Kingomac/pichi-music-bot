c4258bd82302197f5d534515e5241462b425ace4
Kingomac <31375644+Kingomac@users.noreply.github.com>
2023-08-20 17:07:57 +0200

    Spotify support

--- src/cogs/Music.py
+++ src/cogs/Music.py
@@ -1,23 +1,30 @@
 import discord
 from discord.ext import commands
 import wavelink
+from wavelink.ext import spotify
 
 
 class Music(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.voice_client: discord.VoiceClient = None
+        self.spotify_client = spotify.SpotifyClient(
+            client_id="0b595b3e9519444193f4c225df6a2462",
+            client_secret="4036883f133f4f109b647d5ec9af1d70",
+        )
 
-    @commands.command()
-    async def play(self, ctx: commands.Context, *, search: str) -> None:
-        """Simple play command."""
-
+    async def connect_voice_client(self, ctx: commands.Context):
         if not ctx.voice_client:
             self.voice_client: wavelink.Player = await ctx.author.voice.channel.connect(
                 cls=wavelink.Player
             )
         else:
             self.voice_client: wavelink.Player = ctx.voice_client
+
+    @commands.command()
+    async def play(self, ctx: commands.Context, *, search: str) -> None:
+        """Simple play command."""
+        await self.connect_voice_client(ctx)
 
         tracks: list[wavelink.YouTubeTrack] = await wavelink.YouTubeTrack.search(search)
         if not tracks:
@@ -26,6 +33,24 @@ class Music(commands.Cog):
 
         track: wavelink.YouTubeTrack = tracks[0]
         await self.voice_client.play(track)
+
+    @commands.command()
+    async def spotify(self, ctx: commands.Context, *, search: str):
+        await self.connect_voice_client(ctx)
+        decoded = spotify.decode_url(search)
+        if not decoded or decoded["type"] is not spotify.SpotifySearchType.track:
+            await ctx.reply("esto no es de spotify bro", ephemeral=True)
+            return
+        tracks: list[spotify.SpotifyTrack] = await self.spotify_client._search(search)
+        if not tracks:
+            await ctx.reply("url rara", ephemeral=True)
+            return
+        track: spotify.SpotifyTrack = tracks[0]
+
+        if not self.voice_client.is_playing():
+            await self.voice_client.play(track)
+        else:
+            await self.voice_client.queue.put_wait(track)
 
     @commands.command()
     async def disconnect(self, ctx: commands.Context) -> None:
@@ -48,3 +73,8 @@ class Music(commands.Cog):
             and len(self.voice_client.channel.members) == 1
         ):
             await self.voice_client.disconnect()
+
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, ctx: wavelink.TrackEventPayload):
+        if self.voice_client.queue:
+            await self.voice_client.play(await self.voice_client.queue.get_wait())

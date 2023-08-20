77e1da712772d89935ffe3a1d2e929fee1b01da6
Kingomac <31375644+Kingomac@users.noreply.github.com>
2023-08-20 11:43:04 +0200

    Rebuilding from scratch
    Lavalink has insanely better audio quality

--- src/main.py
+++ src/main.py
@@ -1,114 +1,66 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-from music_sources.PlaylistCache import PlaylistCache
-from controllers.MusicController import MusicController
-from music_sources.SongSource import SongSource
+import wavelink
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_GUILDS = os.getenv("DISCORD_GUILDS").strip("[]").split(", ")
 CALL_NAME = "sona" + " "
 print(DISCORD_GUILDS)
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-music_controller = MusicController()
 
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name in DISCORD_GUILDS:
-            print(f"{client.user} está conectado en: {guild.name}")
-        else:
-            print(
-                f"{client.user} está conectado en: {guild.name}, que no está en la lista de servidores admitidos, entonces lo abandona"
-            )
-            guild.leave()
+class Bot(commands.Bot):
+    def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
 
+        super().__init__(intents=intents, command_prefix="+")
 
-@client.event
-async def on_message(message: discord.Message):
-    global voice_client
-    texto: str = message.content
-    print(texto)
-    if not texto.startswith(CALL_NAME) or message.author.name == CALL_NAME:
-        return
-    comando = texto[len(CALL_NAME) :]
-    print(f"comando: {comando}")
+    async def on_ready(self) -> None:
+        print(f"Logged in {self.user} | {self.user.id}")
 
-    if comando == "siguiente":
-        music_controller.stop()
-        music_controller.play()
-    elif comando == "para":
-        music_controller.pause()
-    elif comando == "sigue":
-        music_controller.resume()
-    elif comando == "vete" or comando == "fuera":
-        music_controller.stop()
-        await music_controller.leave_channel()
-    elif comando == "listas":
-        await PlaylistCache.read_list(message=message)
-    elif comando.startswith("descarga "):
-        list_data = list_data.split(" ")
-        list_name = list_data[0]
-        list_url = list_data[1]
-        print(f"list_name = {list_name} ; list_url: {list_url}")
-        source = PlaylistCache(url=list_url, name=list_name)
-        await source.download(message=message)
-
-    elif comando.startswith("canta "):
-        if message.author.voice.channel is None:
-            await message.reply("y donde canto, debajo del mar?? 😩😩")
-            return
-
-        await music_controller.connect_to_voice_channel(
-            voice_channel=message.author.voice.channel
+    async def setup_hook(self) -> None:
+        # Wavelink 2.0 has made connecting Nodes easier... Simply create each Node
+        # and pass it to NodePool.connect with the client/bot.
+        node: wavelink.Node = wavelink.Node(
+            uri="http://localhost:2333", password="youshallnotpass"
         )
-        music_source = comando[len("canta ") :]
-
-        if music_source.startswith("lista "):
-            list_data = music_source[len("lista ") :]
-            if list_data.startswith("https://"):
-                print("https list, let's play")
-                SongSource.get_list_urls(
-                    link=list_data, result_queue=music_controller.song_queue
-                )
-                if not music_controller.is_playing():
-                    music_controller.play()
-            elif list_data in PlaylistCache.get_list().keys():
-                source = PlaylistCache(name=list_data, url="")
-                source.add_playlist_to_queue(result_queue=music_controller.song_queue)
-                print(list(music_controller.song_queue.queue))
-                if not music_controller.is_playing():
-                    music_controller.play()
-            else:
-                await message.reply("y la playlist???")
-                return
-        else:
-            if "list" in music_source and "youtu" in music_source:
-                music_source = music_source.split("?list")[0]
-            elif "list" in music_source:
-                await message.reply(f"no me metas playlists guarrindongo")
-                return
-            try:
-                music_controller.add_to_queue(
-                    SongSource.get_audio_url(link=music_source)
-                )
-                if not music_controller.is_playing():
-                    music_controller.play()
-            except Exception as err:
-                await message.reply(f"que problemas: {str(err)[:1500]}")
+        await wavelink.NodePool.connect(client=self, nodes=[node])
 
 
-@client.event
-async def on_voice_state_update(member, before, after):
-    global voice_client
-    voice_state = member.guild.voice_client
-    if voice_state is not None and len(voice_state.channel.members) == 1:
-        voice_state.stop()
-        await voice_state.disconnect()
+bot = Bot()
 
 
-client.run(DISCORD_TOKEN)
+@bot.command()
+async def play(ctx: commands.Context, *, search: str) -> None:
+    """Simple play command."""
+
+    if not ctx.voice_client:
+        vc: wavelink.Player = await ctx.author.voice.channel.connect(
+            cls=wavelink.Player
+        )
+    else:
+        vc: wavelink.Player = ctx.voice_client
+
+    tracks: list[wavelink.YouTubeTrack] = await wavelink.YouTubeTrack.search(search)
+    if not tracks:
+        await ctx.send(f"Sorry I could not find any songs with search: `{search}`")
+        return
+
+    track: wavelink.YouTubeTrack = tracks[0]
+    await vc.play(track)
+
+
+@bot.command()
+async def disconnect(ctx: commands.Context) -> None:
+    """Simple disconnect command.
+
+    This command assumes there is a currently connected Player.
+    """
+    vc: wavelink.Player = ctx.voice_client
+    await vc.disconnect()
+
+
+bot.run(DISCORD_TOKEN)

b6e9a34b72d1a9e880a4e61d33f8e9426ec9c407
Kingomac <31375644+Kingomac@users.noreply.github.com>
2023-08-20 13:49:11 +0200

    Bot disconnects when noone is in voice channel

--- src/cogs/Music.py
+++ src/cogs/Music.py
@@ -6,17 +6,18 @@ import wavelink
 class Music(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.voice_client: discord.VoiceClient = None
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, search: str) -> None:
         """Simple play command."""
 
         if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(
+            self.voice_client: wavelink.Player = await ctx.author.voice.channel.connect(
                 cls=wavelink.Player
             )
         else:
-            vc: wavelink.Player = ctx.voice_client
+            self.voice_client: wavelink.Player = ctx.voice_client
 
         tracks: list[wavelink.YouTubeTrack] = await wavelink.YouTubeTrack.search(search)
         if not tracks:
@@ -24,7 +25,7 @@ class Music(commands.Cog):
             return
 
         track: wavelink.YouTubeTrack = tracks[0]
-        await vc.play(track)
+        await self.voice_client.play(track)
 
     @commands.command()
     async def disconnect(self, ctx: commands.Context) -> None:
@@ -33,3 +34,17 @@ class Music(commands.Cog):
         """
         vc: wavelink.Player = ctx.voice_client
         await vc.disconnect()
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(
+        self,
+        member: discord.Member,
+        before: discord.VoiceState,
+        after: discord.VoiceState,
+    ):
+        if (
+            self.voice_client != None
+            and self.voice_client.channel != None
+            and len(self.voice_client.channel.members) == 1
+        ):
+            await self.voice_client.disconnect()
